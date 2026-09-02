f283ade91c76c66b853cb2c4d395480dd39650e6
Estéfano Gigena <gemelosaramiel15@gmail.com>
2026-09-02 18:38:38 -0300

    add LoginPage object and use it in valid login test

--- module-2/week-06/saucedemo/test_valid_login.py
+++ module-2/week-06/saucedemo/test_valid_login.py
@@ -1,9 +1,8 @@
 from playwright.sync_api import Page, expect
+from pages.login_page import LoginPage
 
 def test_valid_login(page: Page):
-    page.goto("https://www.saucedemo.com/")
-    page.get_by_placeholder("Username").fill("standard_user")
-    page.get_by_placeholder("Password").fill("secret_sauce")
-    page.get_by_role("button", name="Login").click()
-    
+    login = LoginPage(page)
+    login.open()
+    login.login("standard_user", "secret_sauce")
     expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
